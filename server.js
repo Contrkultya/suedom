7879b2129757fe9280578b50138d847325c02765
Sergo Kosterin <40607257+Contrkultya@users.noreply.github.com>
2021-12-25 04:40:25 +0500

    heroku quickfix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const path = require('path');
 const app = express();
 const db = require("./app/models");
 const bcrypt = require("bcryptjs");
@@ -61,6 +61,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
 });
