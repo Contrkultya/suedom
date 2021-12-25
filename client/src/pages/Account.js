7313a50a6c97cf0448a1bd3141460462ba23e4fc
Sergo Kosterin <40607257+Contrkultya@users.noreply.github.com>
2021-12-25 07:33:53 +0500

    electric boogaloo

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -7,10 +7,7 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
     root: {
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -163,7 +163,7 @@ const Main = observer(() => {
                         <br/>
                         <Typography variant="body2" component="p">
                             {
-                                user.isGoogleAuth ? 'Воспользуйтесь этой функцией, чтобы привязать Google аккаунт, с которым будет синхронизироваться расписание.':
+                                true ? 'Воспользуйтесь этой функцией, чтобы привязать Google аккаунт, с которым будет синхронизироваться расписание.':
                                     'Синхронизация работает '
                             }
                         </Typography>
@@ -172,7 +172,7 @@ const Main = observer(() => {
                     <CardActions className={classes.check}>
                         <Button size="small" variant={'outlined'} onClick={handleGoogleAuth}>
                             {
-                                user.isGoogleAuth ? 'Авторизоваться в Google' : 'Поменять аккаунт'
+                               true ? 'Авторизоваться в Google' : 'Поменять аккаунт'
                             }
                         </Button>
                     </CardActions>
