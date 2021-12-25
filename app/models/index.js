7cb457336ff41851cf3767a20ecad45d478ccd13
Sergo Kosterin <40607257+Contrkultya@users.noreply.github.com>
2021-12-25 05:32:43 +0500

    heroku db ssl fix

--- app/models/index.js
+++ app/models/index.js
@@ -8,6 +8,10 @@ const sequelize = new Sequelize(
     {
         host: config.HOST,
         dialect: config.dialect,
+        dialectOptions: {
+            ssl: true
+        },
+        ssl: true,
         operatorsAliases: false,
 
         pool: {
